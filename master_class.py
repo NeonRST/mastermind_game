fe3ca413faf0cb4294f5e44abb4cfc79d6455439
NeonRST <Ricky0702@live.com>
2023-11-15 14:06:07 +0700

    updated version

--- master_class.py
+++ master_class.py
@@ -2,22 +2,32 @@ import random
 
 
 class Mastermind:
-    def __init__(self, rows, rn, dup):
+    def __init__(self, rows, rn, dup=False):
         self.rows = rows
         self.rn = rn
         self.dup = dup
+        self.temp_ans = ""
+        self.game_history = []
+
+    def menu(self):
         temp_ans = ""
         while len(temp_ans) < self.rows:
             answer = random.randint(1, self.rn)
             if self.dup:
+                temp_ans += str(answer)
+            else:
                 if str(answer) not in temp_ans:
                     temp_ans += str(answer)
-            else:
-                temp_ans += str(answer)
         self.temp_ans = temp_ans
-    
+        print(f"Playing Mastermind with {self.rn} positions and {self.rows} positions")
+        print(f"duplicates set as {self.dup}")
+
     def playing(self):
         guess = input(f"input {self.rows} numbers: ")
+
+        if guess == "hint":
+            print("Hint:")
+            print(f"one of the answers is {self.temp_ans[random.randint(1, (self.rows - 1))]}")
         output = ""
         for i in range(self.rows):
             if guess[i] == self.temp_ans[i]:
@@ -26,9 +36,13 @@ class Mastermind:
                 output += "X"
             else:
                 output += "_"
-        print(output)
+        for i in range(len(output)):
+            print(f"[{output[i]}]", end=" ")
+        self.game_history.append([guess, output])
+        print()
         if guess == self.temp_ans:
             print("you win good job")
+            print()
             return False
         else:
             return True
--- mastermind_main.py
+++ mastermind_main.py
@@ -3,10 +3,27 @@ run = True
 playing = True
 while run:
     print("Rules:")
-    print("")
-    row = int(input("how many rows would you like to play?: "))
+    print("A star 0 indicates that there is a number that is positioned correctly\n"
+          "A letter X indicates that there is a number that is positioned incorrectly\n"
+          "A _ indicates that there a number that is not present\n"
+          "The game is played on until a player successfully solves the puzzle")
+    print()
+    row = int(input("how many positions would you like to play?: "))
     rn = int(input("how many numbers would you like to play?: "))
-    dup = bool(input("would you like duplicates on or off? (True/False): "))
-    ans = master_class.Mastermind(row, rn, dup)
-    while playing:
-        playing = master_class.Mastermind.playing(ans)
+    dup = input("would you like duplicates on or off? (T/F): ")
+    if dup == "T" or dup == "t":
+        dup = True
+    if dup == "F" or dup == "f":
+        dup = False
+    data = master_class.Mastermind(row, rn, dup)
+    ans = master_class.Mastermind.menu(data)
+    in_game = True
+    while in_game:
+        while playing:
+            playing = master_class.Mastermind.playing(data)
+        history = input("would you like to see your play history? (y/n): ")
+        if history == "y" or history == "yes" or history == "Yes":
+            for i in data.game_history:
+                print(f"{i[0]} {i[1]}")
+
+
